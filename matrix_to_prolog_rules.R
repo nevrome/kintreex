c0879f9119ba5ee5b27b5e123bf03fba21bb4fa1
Clemens Schmid <clemens@nevrome.de>
2020-01-15 17:32:18 +0100

    fixed and improved R data prep script

--- matrix_to_prolog_rules.R
+++ matrix_to_prolog_rules.R
@@ -15,7 +15,7 @@ sl_fixed <- sl %>%
     id = fix_names(id)
   ) %>%
   dplyr::arrange(
-    sex
+    sex, id
   )
 
 individuals_facts <- sapply(
@@ -27,11 +27,12 @@ individuals_facts <- sapply(
 
 #### relationships ####
 
-pm <- read.csv("pedigree_matrix.csv", check.names = F)
+pm <- read.csv("pedigree_matrix.csv", check.names = F, row.names = 1)
+pm[upper.tri(pm)] <- t(pm)[upper.tri(pm)]
 
-pm_long <- reshape2::melt(pm) %>%
+pm_long <- reshape2::melt(as.matrix(pm)) %>%
   setNames(c('id.a', 'id.b', 'value')) %>%
-  dplyr::filter(!is.na(value) & value != 100)
+  dplyr::filter(value != 100)
 
 pm_long_fixed <- pm_long %>%
   dplyr::mutate(
@@ -39,35 +40,32 @@ pm_long_fixed <- pm_long %>%
     id.b = fix_names(id.b),
     value_cut = cut(
       value, 
-      breaks = c(101, 26, 12.6, 6.3, 1, -1), 
+      breaks = c(101, 50, 25, 12.5, 6.25, -1), 
       labels = c(
         "first_degree",
         "second_degree",
         "third_degree",
         "fourth_degree",
         "unrelated"
-      ) %>% rev()
+      ) %>% rev(),
+      include.lowest = TRUE,
+      right = FALSE
       )
   ) %>%
   dplyr::arrange(
-    value
+    value, id.a
   )
 
-relationship_facts <- lapply(
+relationship_facts <- sapply(
   1:nrow(pm_long_fixed), function (i) {
-    c(
       paste0(
         pm_long_fixed$value_cut[i], "(", 
           pm_long_fixed$id.a[i], ", ", pm_long_fixed$id.b[i], 
         ")."
-      ), 
-      paste0(
-        pm_long_fixed$value_cut[i], "(", 
-          pm_long_fixed$id.b[i], ", ", pm_long_fixed$id.a[i], 
-        ")."
       )
-    )
-}) %>% unlist()
+})
+
+
 
 #### write facts to file ####
 
