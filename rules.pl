627e2c377615261d86a47c7e8b882bb5e3bc6897
Clemens Schmid <clemens@nevrome.de>
2020-01-17 14:48:26 +0100

    experiments with more rules

--- rules.pl
+++ rules.pl
@@ -1,5 +1,15 @@
 /* Rules */
 
+all_relations(F,M,B,S,So,Da,Y):-
+   (father_of(F,Y), F \== B, F \== Da;
+    brother_of(B,Y), B \== F, B \== Da;
+    son_of(So,Y), So \== F, So \== B;
+    false),
+   (mother_of(M,Y), M \== B, M \== Da;
+    sister_of(S,Y), S \== F, S \== Da;
+    daughter_of(Da,Y), Da \== M, Da \== S;
+    false).
+
 % parents
 father_of(F,Y):- male(F), first_degree(F,Y).
 mother_of(M,Y):- female(M), first_degree(M,Y).
@@ -7,7 +17,13 @@ mother_of(M,Y):- female(M), first_degree(M,Y).
 parent_of(P,Y):- father_of(P,Y).
 parent_of(P,Y):- mother_of(P,Y).
 
-parents_of(M,F,Z):- mother_of(M,Z), father_of(F,Z).
+parents_of(F,M,Y):- father_of(F,Y), mother_of(M,Y).
+
+% children
+child_of(Y,P):- parent_of(P,Y).
+child_of(Y,F,M):- parents_of(F,M,Y).
+son_of(Y,P):- male(Y), child_of(Y,P).
+daughter_of(Y,P):- female(Y), child_of(Y,P).
 
 % siblings
 brother_of(B,Y):- 
